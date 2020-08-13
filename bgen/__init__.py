747f82d0021abc603364ab07e832a4fc32fb9531
Danilo Horta <danilo.horta@pm.me>
2020-08-13 05:07:51 +0100

    scm to track version

--- bgen/__init__.py
+++ bgen/__init__.py
@@ -1,3 +1,5 @@
+from importlib import import_module as _import_module
+
 try:
     from ._ffi import ffi
 
@@ -10,7 +12,11 @@ and reinstall the package again."""
 
     raise RuntimeError(str(e) + _ffi_err)
 
+try:
+    __version__ = getattr(_import_module("iseq_prof._version"), "version", "x.x.x")
+except ModuleNotFoundError:
+    __version__ = "x.x.x"
+
 __all__ = [
     "__version__",
-    "test",
 ]
