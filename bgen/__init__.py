63b3b39b1d20cb532d47bab13ea212ef0095552b
Danilo Horta <danilo.horta@pm.me>
2020-08-13 05:03:34 +0100

    first __init__.py

---
+++ bgen/__init__.py
@@ -0,0 +1,16 @@
+try:
+    from ._ffi import ffi
+
+    del ffi
+except Exception as e:
+    _ffi_err = """
+It is likely caused by a broken installation of this package.
+Please, make sure you have a C compiler and try to uninstall
+and reinstall the package again."""
+
+    raise RuntimeError(str(e) + _ffi_err)
+
+__all__ = [
+    "__version__",
+    "test",
+]
