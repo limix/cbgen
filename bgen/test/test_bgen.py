17bde4b20d9f9ee738ddcbbc5509677b280439f6
Danilo Horta <danilo.horta@pm.me>
2020-08-15 19:55:54 +0100

    Tests with corrupted metafile

--- bgen/test/test_bgen.py
+++ bgen/test/test_bgen.py
@@ -2,12 +2,91 @@ from pathlib import Path
 
 import pytest
 from numpy import nan
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 
 from bgen import bgen_file, bgen_metafile, example
 
 
-def test_bgen_samples_not_present(tmp_path: Path):
+def test_bgen_invalid_metafile():
+    mfilepath = example.get("wrong.metadata")
+    with pytest.raises(RuntimeError):
+        bgen_metafile(mfilepath)
+
+    mfilepath = example.get("haplotypes.bgen.metadata.corrupted")
+    with pytest.raises(RuntimeError):
+        bgen_metafile(mfilepath)
+
+
+def test_bgen_phased_genotype(tmp_path):
+    filepath = example.get("haplotypes.bgen")
+    mfilepath = tmp_path / f"{filepath.name}.metafile"
+
+    bgen = bgen_file(filepath)
+    assert bgen.filepath.name == "haplotypes.bgen"
+    assert bgen.nvariants == 4
+    assert bgen.nsamples == 4
+    assert bgen.contain_samples
+    samples = bgen.read_samples()
+    assert_array_equal(samples, [b"sample_0", b"sample_1", b"sample_2", b"sample_3"])
+
+    bgen.create_metafile(mfilepath, verbose=False)
+
+    mf = bgen_metafile(mfilepath)
+
+    assert mf.filepath.name == mfilepath.name
+    assert mf.npartitions == 1
+    assert mf.nvariants == 4
+    assert mf.partition_size == 4
+
+    part = mf.read_partition(0)
+
+    assert part.variants.id[0] == b"SNP1"
+    assert part.variants.rsid[0] == b"RS1"
+    assert part.variants.chrom[0] == b"1"
+    assert part.variants.position[0] == 1
+    assert part.variants.nalleles[0] == 2
+    assert part.variants.allele_ids[0] == b"A,G"
+    voff = part.variants.offset[0]
+    gt = bgen.read_genotype(voff)
+    assert_allclose(
+        gt.probs,
+        [
+            [1.0, 0.0, 1.0, 0.0],
+            [0.0, 1.0, 1.0, 0.0],
+            [1.0, 0.0, 0.0, 1.0],
+            [0.0, 1.0, 0.0, 1.0],
+        ],
+    )
+    assert gt.phased
+    assert_allclose(gt.ploidy, [2, 2, 2, 2])
+    assert_allclose(gt.missing, [False, False, False, False])
+
+    assert part.variants.id[3] == b"SNP4"
+    assert part.variants.rsid[3] == b"RS4"
+    assert part.variants.chrom[3] == b"1"
+    assert part.variants.position[3] == 4
+    assert part.variants.nalleles[3] == 2
+    assert part.variants.allele_ids[3] == b"A,G"
+    voff = part.variants.offset[3]
+    gt = bgen.read_genotype(voff)
+    assert_allclose(
+        gt.probs,
+        [
+            [0.0, 1.0, 0.0, 1.0],
+            [1.0, 0.0, 1.0, 0.0],
+            [0.0, 1.0, 1.0, 0.0],
+            [1.0, 0.0, 0.0, 1.0],
+        ],
+    )
+    assert gt.phased
+    assert_allclose(gt.ploidy, [2, 2, 2, 2])
+    assert_allclose(gt.missing, [False, False, False, False])
+
+    mf.close()
+    bgen.close()
+
+
+def test_bgen_complex_unphased(tmp_path: Path):
     filepath = example.get("complex.23bits.no.samples.bgen")
     mfilepath = tmp_path / f"{filepath.name}.metafile"
     with bgen_file(filepath) as bgen:
