7d7f710d8d43a665a7f02c5d545a85d9f53f5eb0
Danilo Horta <danilo.horta@pm.me>
2020-08-16 02:09:19 +0100

    +merged_487400x4840000.bgen, clang-format

--- bgen/example.py
+++ bgen/example.py
@@ -19,6 +19,7 @@ goodboy = pooch.create(
         "wrong.metadata": "f746345605150076f3234fbeea7c52e86bf95c9329b2f08e1e3e92a7918b98fb",
         "merged_487400x220000.bgen": "8dccd89a53e048ea24305cb04c3653b0eb2af265b04d05a60cfa2cca5fb7ae94",
         "merged_487400x2420000.bgen": "81aecfab787bee1cb7f1d0d21f2465c581a4db78011d8b0f0f73c868e17ec888",
+        "merged_487400x4840000.bgen": "5ef82f92a001615c93bbb317a9fd2329272370c6d481405d4f8f0a2b7fddf68b",
     },
 )
 
