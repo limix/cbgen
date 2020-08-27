8ff2bd6be6a57f07da6151a1732daf1f94a171c0
Danilo Horta <danilo.horta@pm.me>
2020-08-27 23:43:18 +0100

    blake hash

--- cbgen/example.py
+++ cbgen/example.py
@@ -18,7 +18,7 @@ goodboy = pooch.create(
         "haplotypes.bgen.metadata.corrupted": "8f55628770c1ae8155c1ced2463f15df80d32bc272a470bb1d6b68225e1604b1",
         "haplotypes.bgen.metafile": "7e8b13aed04e2166649dde2c1c44aa4239f158aef7cf06d69c51113b9a2ae175",
         "wrong.metadata": "f746345605150076f3234fbeea7c52e86bf95c9329b2f08e1e3e92a7918b98fb",
-        "merged_487400x220000.bgen": "8dccd89a53e048ea24305cb04c3653b0eb2af265b04d05a60cfa2cca5fb7ae94",
+        "merged_487400x220000.bgen": "blake2b:2bf8043907eff9c00021dd044d80d63873f465898b8732c8bcbb533ca5fcd63875aa54e8103768631610f76c4c82577bc7c15f7bea90bb1ab1906b12ceddf56e",
         "merged_487400x2420000.bgen": "81aecfab787bee1cb7f1d0d21f2465c581a4db78011d8b0f0f73c868e17ec888",
         "merged_487400x4840000.bgen": "5ef82f92a001615c93bbb317a9fd2329272370c6d481405d4f8f0a2b7fddf68b",
     },
