9a1d69adabd3d6c1399c5dfdeee1b7e66d95de3a
Danilo Horta <danilo.horta@pm.me>
2022-07-01 23:08:34 +0100

    Revive wheel builds.
    Missing Windows builds until poetry 1.2.0b3 or newer is released.
    I believe they have applied a fix in their main branch that
    fix failure at the poetry build environment step (newer in the 1.2.x but
    very useful as it allows cmake integration into the build).

--- build_ext.py
+++ build_ext.py
@@ -1,39 +1,113 @@
 import os
 import platform
-from os.path import join
+import shutil
+import subprocess
+import tarfile
+import urllib.request
+from pathlib import Path
 from typing import List
 
 from cffi import FFI
+from cmake import CMAKE_BIN_DIR
 
 ffibuilder = FFI()
-libs = ["bgen", "elapsed", "athr", "zstd"]
+
+libs = ["bgen", "athr", "elapsed"]
 
 if platform.system() == "Windows":
-    libs += ["zlib"]
+    libs += ["zstd_static", "zlibstatic"]
 else:
-    libs += ["z"]
+    libs += ["zstd", "z"]
 
-folder = os.path.dirname(os.path.abspath(__file__))
+pwd = Path(os.path.dirname(os.path.abspath(__file__)))
 
-with open(join(folder, "cbgen", "interface.h"), "r") as f:
+
+def rm(folder: Path, pattern: str):
+    for filename in folder.glob(pattern):
+        filename.unlink()
+
+
+def build_deps(pwd: Path, user: str, project: str, version: str):
+    ext_dir = pwd / ".ext_deps"
+    shutil.rmtree(ext_dir, ignore_errors=True)
+
+    prj_dir = ext_dir / f"{project}-{version}"
+    build_dir = prj_dir / "build"
+    os.makedirs(build_dir, exist_ok=True)
+
+    url = f"https://github.com/{user}/{project}/archive/refs/tags/v{version}.tar.gz"
+
+    with urllib.request.urlopen(url) as rf:
+        data = rf.read()
+
+    tar_filename = f"{project}-{version}.tar.gz"
+
+    with open(ext_dir / tar_filename, "wb") as lf:
+        lf.write(data)
+
+    with tarfile.open(ext_dir / tar_filename) as tf:
+        tf.extractall(ext_dir)
+
+    cmake_bin = str(next(Path(CMAKE_BIN_DIR).glob("cmake*")))
+    subprocess.check_call(
+        [
+            cmake_bin,
+            "-S",
+            str(prj_dir),
+            "-B",
+            str(build_dir),
+            "-DCMAKE_BUILD_TYPE=Release",
+            "-DENABLE_ALL_WARNINGS=ON",
+            "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+        ],
+    )
+    subprocess.check_call([cmake_bin, "--build", str(build_dir), "--config", "Release"])
+    subprocess.check_call(
+        [cmake_bin, "--install", str(build_dir), "--prefix", str(ext_dir)]
+    )
+    rm(ext_dir / "lib", "*.dylib")
+    rm(ext_dir / "lib", "*.so*")
+    rm(ext_dir / "lib64", "*.dylib")
+    rm(ext_dir / "lib64", "*.so*")
+
+    extra_libs = set()
+    for line in open(build_dir / "CMakeCache.txt", "r"):
+        line = line.strip()
+        if line.startswith("CURSES_CURSES_LIBRARY:"):
+            if not line.endswith("NOTFOUND"):
+                extra_libs.add("curses")
+        elif line.startswith("CURSES_NCURSES_LIBRARY:"):
+            if not line.endswith("NOTFOUND"):
+                extra_libs.add("ncurses")
+
+    if len(extra_libs) > 1:
+        extra_libs.remove("curses")
+
+    return list(extra_libs)
+
+
+if not os.getenv("BGEN_SKIP_BUILD_DEPS", False):
+    libs += build_deps(pwd, "limix", "bgen", "4.1.9")
+
+with open(pwd / "cbgen" / "interface.h", "r") as f:
     ffibuilder.cdef(f.read())
 
-with open(join(folder, "cbgen", "genotype.h"), "r") as f:
+with open(pwd / "cbgen" / "genotype.h", "r") as f:
     ffibuilder.cdef(f.read())
 
-with open(join(folder, "cbgen", "genotype.c"), "r") as f:
+with open(pwd / "cbgen" / "genotype.c", "r") as f:
     genotype_c = f.read()
 
-with open(join(folder, "cbgen", "partition.h"), "r") as f:
+with open(pwd / "cbgen" / "partition.h", "r") as f:
     ffibuilder.cdef(f.read())
 
-with open(join(folder, "cbgen", "partition.c"), "r") as f:
+with open(pwd / "cbgen" / "partition.c", "r") as f:
     partition_c = f.read()
 
-with open(join(folder, "cbgen", "samples.h"), "r") as f:
+with open(pwd / "cbgen" / "samples.h", "r") as f:
     ffibuilder.cdef(f.read())
 
-with open(join(folder, "cbgen", "samples.c"), "r") as f:
+with open(pwd / "cbgen" / "samples.c", "r") as f:
     samples_c = f.read()
 
 extra_link_args: List[str] = []
@@ -42,7 +116,7 @@ if "BGEN_EXTRA_LINK_ARGS" in os.environ:
 
 ffibuilder.set_source(
     "cbgen._ffi",
-    fr"""
+    rf"""
     #include "bgen/bgen.h"
     {genotype_c}
     {partition_c}
@@ -51,6 +125,8 @@ ffibuilder.set_source(
     libraries=libs,
     extra_link_args=extra_link_args,
     language="c",
+    library_dirs=[str(pwd / ".ext_deps" / "lib"), str(pwd / ".ext_deps" / "lib64")],
+    include_dirs=[str(pwd / ".ext_deps" / "include")],
 )
 
 if __name__ == "__main__":
