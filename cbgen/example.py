625c02fe430544a04ea9aaa72203a854e7ea5553
Danilo Horta <danilo.horta@pm.me>
2023-11-09 00:07:24 +0000

    File repo by Carl: https://github.com/limix/bgen-reader-py/pull/49

--- cbgen/example.py
+++ cbgen/example.py
@@ -11,7 +11,7 @@ pooch.get_logger().setLevel(logging.ERROR)
 
 goodboy = pooch.create(
     path=BGEN_CACHE_HOME / "test_data",
-    base_url="https://bgen-examples.s3.amazonaws.com/",
+    base_url="https://github.com/fastlmm/bgen-sample-files/raw/main",
     registry={
         "complex.23bits.no.samples.bgen": "25d30a4e489da1aeb05f9893af98e8bf3b09d74db2982bf1828f8c8565886fc6",
         "haplotypes.bgen": "84e0b59efcc83c7c305cf5446e5dc26b49b15aeb4157a9eb36451376ce3efe4c",
